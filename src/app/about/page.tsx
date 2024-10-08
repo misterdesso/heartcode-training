3f08afbe2939e297a8a20adb94780a15b9b69ed6
misterdesso <jtientf2@gmail.com>
2024-10-08 16:19:44 +0800

    draft2.1

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,8 +4,8 @@ import Lethal_Company from "../assets/test.png"
 export default function AboutMe() {
   return (
     <div>
-      <div>I'm Jin Kai</div>
-      <Image src={Lethal_Company} width={1000} height={1000} alt="test" />
+      <div>I am Jin Kai</div>
+      <Image src={Lethal_Company} width={1000} height={1000} alt="Lethal Company" />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen flex items-center justify-center bg-blue-100">
-      <p className="font-bold text-4xl">Don't Do Drugs, Okay?</p>
+      <p className="font-bold text-4xl">Do Not Do Drugs, Okay?</p>
     </div>
   );
 }
