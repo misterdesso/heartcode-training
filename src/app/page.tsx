d42f8790786a6af4c1560c5653320da92af2a140
Desso <desso@Desso-MBA.local>
2024-10-08 13:27:46 +0800

    image2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import NavBar from "@/components/navbar-menu/navbar"
-
-
 export default function Home() {
   return (
     <div>
-      {/* <NavBar />  */}
       <div>Hello world again</div>
     </div>
   );
